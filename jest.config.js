90c78440409867e5c048ee550a27f256efcc221d
yazalde filimone <yazaldefilimon@gmail.com>
2022-08-07 17:25:27 +0200

    chore: jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,16 @@
 module.exports = {
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}', '!<rootDir>/src/main/**/*', '!<rootDir>/src/**/index.ts', '!**/*.d.ts'],
+  coverageDirectory: 'coverage',
+  setupFilesAfterEnv: ['<rootDir>/src/shared/settings/jest-setup.ts'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
   testEnvironment: 'jsdom',
-  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
-  collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)'],
-  setupFilesAfterEnv: ['<rootDir>./jest/setup.ts']
+  transform: {
+    '.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '~/tests/(.*)': '<rootDir>/tests/$1',
+    '~/(.*)': '<rootDir>/src/$1'
+    // '\\.scss$': 'identity-obj-proxy'
+  }
 };
